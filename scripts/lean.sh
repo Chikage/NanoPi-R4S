d7b24e357fcc74ad951a42f738e38ecd0fc1941a
Shiryee Lin <Chikage@live.cn>
2021-06-30 01:45:45 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 pushd package/lean
+rm -rf helloworld
 git clone --depth=1 https://github.com/fw876/helloworld
 rm -rf luci-theme-argon
+rm -rf luci-app-argon-config
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone -b master https://github.com/jerrykuku/luci-app-argon-config.git
 rm -rf luci-app-docker
+rm -rf luci-lib-docker
 git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
 git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 popd
