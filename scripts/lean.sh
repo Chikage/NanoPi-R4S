da3cebdf2103986bf083e07a3cc62c8b5d17f5e6
Shiryee Lin <Chikage@live.cn>
2021-06-29 11:16:35 +0800

    Rewrite

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -7,4 +7,9 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone -b master https://github.com/jerrykuku/luci-app-argon-config.git
 popd
 
+mkdir package/community
+pushd package/community
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+popd
+
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
