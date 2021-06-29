c8ce771dff4668e47806ac40c63cf5da980735cf
Shiryee Lin <Chikage@live.cn>
2021-06-29 22:32:20 +0800

    Merge Patches

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -5,6 +5,9 @@ git clone --depth=1 https://github.com/fw876/helloworld
 rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone -b master https://github.com/jerrykuku/luci-app-argon-config.git
+rm -rf luci-app-docker
+git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 popd
 
 mkdir package/community
@@ -12,4 +15,12 @@ pushd package/community
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 popd
 
+git am $GITHUB_WORKSPACE/patches/*.patch
+echo -e " OpenWrt Built on "$(date +%Y.%m.%d)"\n -----------------------------------------------------" >> package/base-files/files/etc/banner
+
+pushd package/lean/autocore/files/arm/sbin
+cp -f $GITHUB_WORKSPACE/scripts/cpuinfo cpuinfo
+popd
+
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
