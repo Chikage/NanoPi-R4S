e814e4a7dffd3164d2eeacb7c433760f351fbd6e
Shiryee Lin <Chikage@live.cn>
2021-06-30 02:02:04 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -7,13 +7,12 @@ rm -rf luci-app-argon-config
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone -b master https://github.com/jerrykuku/luci-app-argon-config.git
 rm -rf luci-app-docker
-rm -rf luci-lib-docker
-git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
-git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 popd
 
 mkdir package/community
 pushd package/community
+git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 popd
 
